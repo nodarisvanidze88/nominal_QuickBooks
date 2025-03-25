d7fd377001e2deab2bef86f7d16ff67c964571e7
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-03-26 00:36:00 +0400

    install sqlAlchemy and dotenv and implement seesion.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routes import auth_routes, account_routes
 
 
-
-app = FastAPI(title="API for the project", 
-              description="This is a very fancy project, with auto docs for the API and everything", 
+app = FastAPI(title="OAuth-based API Integration with QuickBooks Online", 
+              description="API Integration with QuickBooks Online", 
               version="0.0.1")
 
 app.add_middleware(CORSMiddleware, 
@@ -12,3 +12,6 @@ app.add_middleware(CORSMiddleware,
                    allow_credentials=True, 
                    allow_methods=["*"], 
                    allow_headers=["*"])
+
+app.include_router(auth_routes.router)
+app.include_router(account_routes.router)
