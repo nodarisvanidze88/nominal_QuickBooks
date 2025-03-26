aaef7b8af1716401fef400ac835569c153d1ab34
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-03-26 05:27:10 +0400

    implemented docker files

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import auth_routes, account_routes
 
-
 app = FastAPI(title="OAuth-based API Integration with QuickBooks Online", 
               description="API Integration with QuickBooks Online", 
               version="0.0.1")
