7803c7de6cac422d03a41ca044df65a5ab33ebed
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-03-29 02:12:07 +0400

    create logger to loggly

--- utils/logger.py
+++ utils/logger.py
@@ -1,10 +1,14 @@
 import logging
+from loggly.handlers import HTTPSHandler
+from core.config import LOGGLY_TOKEN
+
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(name)s | %(message)s")
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        logger.setLevel(logging.INFO)
+        https_handler = HTTPSHandler(url=f"https://logs-01.loggly.com/inputs/{LOGGLY_TOKEN}/tag/python")
+        formatter = logging.Formatter("%(levelname)s | %(asctime)s | %(name)s | %(message)s")
+        https_handler.setFormatter(formatter)
+        logger.addHandler(https_handler)
     return logger
